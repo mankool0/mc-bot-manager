87e31dda3da041e2cb49a785de2ec5079fa5a321
mankool0 <8884398+mankool0@users.noreply.github.com>
2025-09-26 15:32:32 -0700

    Initial Commit

---
+++ client/src/client/java/me/mankool/mcBotClient/client/McBotClientClient.java
@@ -0,0 +1,10 @@
+package me.mankool.mcBotClient.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class McBotClientClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ client/src/main/java/me/mankool/mcBotClient/McBotClient.java
@@ -0,0 +1,10 @@
+package me.mankool.mcBotClient;
+
+import net.fabricmc.api.ModInitializer;
+
+public class McBotClient implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
