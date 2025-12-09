d4d11025a2d52e164c26d9e6e95bbcbaa180f842
mankool0 <8884398+mankool0@users.noreply.github.com>
2025-12-09 14:52:02 -0800

    add chunk data sending to client

---
+++ client/src/main/java/mankool/mcBotClient/mixin/client/ClientboundSectionBlocksUpdatePacketAccessor.java
@@ -0,0 +1,20 @@
+package mankool.mcBotClient.mixin.client;
+
+import net.minecraft.core.SectionPos;
+import net.minecraft.network.protocol.game.ClientboundSectionBlocksUpdatePacket;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ClientboundSectionBlocksUpdatePacket.class)
+public interface ClientboundSectionBlocksUpdatePacketAccessor {
+
+    @Accessor("sectionPos")
+    SectionPos getSectionPos();
+
+    @Accessor("positions")
+    short[] getPositions();
+
+    @Accessor("states")
+    BlockState[] getStates();
+}
